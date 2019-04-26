0a5abe2b0e928c7989847d3d00657f7c2cd616d8
bartadam <bart.adamiak@gmail.com>
2019-04-26 16:47:35 +0200

    Menu

--- app/components/stock.js
+++ app/components/stock.js
@@ -1,4 +1,160 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    ingre: [{
+        ingre: "Ser żółty",
+        counter: 50
+    },
+    {
+        ingre: "Szynka",
+        counter: 20
+    },
+    {
+        ingre: "Tuńczyk",
+        counter: 10
+    },
+    {
+        ingre: "Ogórki",
+        counter: 10,
+    },
+    {
+        ingre: "Ser kozi",
+        counter: 7
+    },
+    {
+        ingre: "Ser owczy",
+        counter: 7
+    },
+    {
+        ingre: "Ser pleśniowy",
+        counter: 3
+    },
+    {
+        ingre: "Krewetki",
+        counter: 2
+    },
+    {
+        ingre: "Oliwki",
+        counter: 2
+    },
+    {
+        ingre: "Papryka chili",
+        counter: 1
+    },
+    {
+        ingre: "Pieprz cayenne",
+        counter: 0.5
+    },
+    {
+        ingre: "Pomidory",
+        counter: 20
+    },
+    {
+        ingre: "Czosnek",
+        counter: 3
+    },
+    {
+        ingre: "Salami",
+        counter: 25
+    },
+    {
+        ingre: "Rukola",
+        counter: 1
+    },
+    {
+        ingre: "Jalapeno",
+        counter: 2
+    },
+    {
+        ingre: "Fasola",
+        counter: 2
+    },
+    {
+        ingre: "Bób",
+        counter: 2
+    },
+    {
+        ingre: "Szpinak",
+        counter: 2
+    }
+]
+    // model() {
+    //     return [{
+    //         ingre: "Ser żółty",
+    //         counter: 50
+    //     },
+    //     {
+    //         ingre: "Szynka",
+    //         counter: 20
+    //     },
+    //     {
+    //         ingre: "Tuńczyk",
+    //         counter: 10
+    //     },
+    //     {
+    //         ingre: "Ogórki",
+    //         counter: 10,
+    //     },
+    //     {
+    //         ingre: "Ser kozi",
+    //         counter: 7
+    //     },
+    //     {
+    //         ingre: "Ser owczy",
+    //         counter: 7
+    //     },
+    //     {
+    //         ingre: "Ser pleśniowy",
+    //         counter: 3
+    //     },
+    //     {
+    //         ingre: "Krewetki",
+    //         counter: 2
+    //     },
+    //     {
+    //         ingre: "Oliwki",
+    //         counter: 2
+    //     },
+    //     {
+    //         ingre: "Papryka chili",
+    //         counter: 1
+    //     },
+    //     {
+    //         ingre: "Pieprz cayenne",
+    //         counter: 0.5
+    //     },
+    //     {
+    //         ingre: "Pomidory",
+    //         counter: 20
+    //     },
+    //     {
+    //         ingre: "Czosnek",
+    //         counter: 3
+    //     },
+    //     {
+    //         ingre: "Salami",
+    //         counter: 25
+    //     },
+    //     {
+    //         ingre: "Rukola",
+    //         counter: 1
+    //     },
+    //     {
+    //         ingre: "Jalapeno",
+    //         counter: 2
+    //     },
+    //     {
+    //         ingre: "Fasola",
+    //         counter: 2
+    //     },
+    //     {
+    //         ingre: "Bób",
+    //         counter: 2
+    //     },
+    //     {
+    //         ingre: "Szpinak",
+    //         counter: 2
+    //     }
+    // ]
+    // }
 });
--- app/routes/pizzeria1.js
+++ app/routes/pizzeria1.js
@@ -5,43 +5,54 @@ export default Route.extend({
         return [{
             pizza: "Warszawska",
             price: "25.00",
+            ingredients: ["ser", " sos pomidorowy", " ogórki"]
         }, 
         {
             pizza: "Serowa",
             price: "27.50",
+            ingredients: ["ser", " sos pomidorowy", " sery: kozi, owczy, pleśniowy"]
         },
         {
             pizza: "Hawajska",
-            price: "29.00"
+            price: "29.00",
+            ingredients: ["ser", " sos pomidorowy", " ananas", " szynka"]
         },
         {
             pizza: "Morska",
             price: "29.00",
+            ingredients: ["ser", " sos pomidorowy", " tuńczyk", " krewetki", " oliwki"],
         },
         {
             pizza: "Ostra",
             price: "33.00",
+            ingredients: ["ser", " sos chili", " papryka chili", ' salami'],
         },
         {
             pizza: "Szpinakowa",
             price: "33.00",
+            ingredients: ["ser", " sos czosnkowy", " szpinak", ' pomidor'],
         },
         {
             pizza: "Bardzo ostra",
             price: "35.00",
+            ingredients: ["ser", " sos chili", " pieprz cayenne", " salami", " rukola"],
         },
         {
             pizza: "Bardzo Bardzo ostra",
-            price: "42.00"
+            price: "42.00",
+            ingredients: ["ser", " sos chili", " pieprz cayenne", " papryczki jalapeno", " szynka"]
         },
         {
             pizza: "Wegańska",
             price: "42.00",
+            ingredients: ["tofu", " sos pomidorowy", " ogórki", " fasola", " bób", " szpinak", " czosnek"],
         },
         {
             pizza: "Giga",
-            price: "55.00"
+            price: "55.00",
+            ingredients: "30 losowo wybranych składników"
         }
     ]
-    }
+    },
+  
 });
